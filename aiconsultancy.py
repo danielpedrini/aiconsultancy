184a9b942e5cb5f7c8b784c8e98501a63d41e667
Daniel Pedrini <daniel.pedrini@gmail.com>
2023-08-28 09:19:54 -0300

    add image processing

--- aiconsultancy.py
+++ aiconsultancy.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.0.1'
 
 def getversion():
     print(f'{__version__}')
@@ -95,3 +95,34 @@ def tf_getModelMemoryUsage(batch_size, model):
         print("Memory for", out_shape, " layer in MB is:", single_layer_mem_MB)
         features_mem += single_layer_mem_MB  #Add to total feature memory count
 
+# Image processing
+
+def img_filtering(img, filter):
+    import cv2
+    import numpy as np
+    
+    identity = np.array(([0, 0, 0],[0, 1, 0], [0, 0, 0]), np.float32)
+    edge_1 = np.array(([0, -1, 0],[-1, 4, -1],[0, -1, 0]), np.float32)
+    edge_2 = np.array(([-1, -1, -1],[-1, 8, -1],[-1, -1, -1]), np.float32)
+    sharpen = np.array(([0, -1, 0],[-1, 5, -1], [0, -1, 0]), np.float32)
+    box_blur = np.array(([1, 1, 1],[1, 1, 1], [1, 1, 1]), np.float32)/9
+    gaussing_blur_3 = np.array(([1, 2, 1],[2, 4, 2], [1, 2, 1]), np.float32)/16
+    gaussing_blur_5 = np.array(([1, 4, 6, 4, 1], [4, 16, 24, 16, 4], [6, 24, 36, 24, 6], [4, 16, 24, 16, 4], [1, 4, 6, 4, 1]), np.float32)/256
+
+    if filter == 'edge_1':
+        k = edge_1
+    elif filter == 'edge_2':
+        k = edge_2
+    elif filter == 'sharpen':
+        k = sharpen
+    elif filter == 'box_blur':
+        k = box_blur
+    elif filter == 'gaussing_blur_3':
+        k = gaussing_blur_3
+    elif filter == 'gaussing_blur_5':
+        k = gaussing_blur_5
+    else:
+        k = identity
+
+    filtered_img = cv2.filter2D(img, -1, k)
+    return filtered_img
